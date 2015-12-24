cd9581e03ae57c59f703b3484ab9f77babefab77
notreami <notreami@163.com>
2015-12-24 14:27:23 +0800

    更改MultipartConfig上传的name,增加Fiiter异步支持,否则传递过来的request就是不支持异步调用的说明。

--- src/main/java/com/notreami/student1/annotation/ServletAnnotationTest.java
+++ src/main/java/com/notreami/student1/annotation/ServletAnnotationTest.java
@@ -17,6 +17,23 @@ import java.util.Date;
 /**
  * Created by zhaobo04 on 15/11/30.
  */
+/**
+ *可插性支持,动态注册为一个可以用于服务的 Servlet
+ * Servlet3.0支持使用注解配置Servlet。我们只需在Servlet对应的类上使用@WebServlet进行标注，
+ * 我们就可以访问到该Servlet了，而不需要再在web.xml文件中进行配置。@WebServlet的urlPatterns
+ * 和value属性都可以用来表示Servlet的部署路径，它们都是对应的一个数组。
+ */
+/**
+ * 支持异步返回的Servlet
+ * 对于Servlet的异步返回，首先我们必须指定@WebServlet的asyncSupported属性为true（默认是false），同时在它之前的Filter
+ * 的asyncSupported属性也必须是true，否则传递过来的request就是不支持异步调用的。
+ *
+ */
+/**
+ * 带初始化参数的Servlet
+ * WebServlet的属性initParams可以用来指定当前Servlet的初始化参数，它是一个数组，
+ * 里面每一个@WebInitParam表示一个参数。
+ */
 @WebServlet(urlPatterns = {"/servletAnnotationTest"}, asyncSupported = true, loadOnStartup = 1, name = "ServletAnnotationTest", displayName = "描述",
         initParams = {@WebInitParam(name = "username", value = "ServletAnnotationTest")})
 public class ServletAnnotationTest extends HttpServlet {
