b7d27f29097293ef9d919f86e1811da6b6fe1e8f
notreami <notreami@163.com>
2015-12-29 14:08:38 +0800

    web-fragment

---
+++ fragmentB/webapp/web/fragment/FragmentTest.java
@@ -0,0 +1,29 @@
+package web.fragment;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by notreami on 15/12/29.
+ */
+@WebServlet(urlPatterns = {"/fragmentTest"}, asyncSupported = false, loadOnStartup = 1, name = "fragmentTest", displayName = "描述")
+public class FragmentTest  extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("FragmentTest doGet");
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("FragmentTest doPost");
+
+        req.getRequestDispatcher("fragmentB.jsp").forward(req, resp);
+    }
+}
