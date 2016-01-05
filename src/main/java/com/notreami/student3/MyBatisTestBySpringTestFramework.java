bc282eca3983d25ab952bfcb18904532e6575ed0
notreami <notreami@163.com>
2016-01-05 14:25:53 +0800

    集成github上开源的MyBatis翻页插件项目-Mybatis_PageHelper,使用ISelect接口可以参考如下用法(返回值为Page或PageInfo)

--- src/main/java/com/notreami/student3/MyBatisTestBySpringTestFramework.java
+++ src/main/java/com/notreami/student3/MyBatisTestBySpringTestFramework.java
@@ -1,5 +1,7 @@
 package com.notreami.student3;
 
+import com.github.pagehelper.ISelect;
+import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.notreami.student3.generatordomain.User;
@@ -29,6 +31,7 @@ public class MyBatisTestBySpringTestFramework {
 
     @Autowired
     private SqlSession sqlSession;//注入SqlSession
+
     @Test
     public void testAddUser() {
         User user = new User();
@@ -71,7 +74,7 @@ public class MyBatisTestBySpringTestFramework {
     @Test
     public void testGetAllUserPageToPageInfo() {
         //第二种，Mapper接口方式的调用，推荐这种使用方式。
-        PageHelper.startPage(2, 1);
+        PageHelper.startPage(2, 2);
         List<User> users = userService.getAllUser();
         //用PageInfo对结果进行包装
         PageInfo page = new PageInfo(users);
@@ -90,5 +93,44 @@ public class MyBatisTestBySpringTestFramework {
         for (User user : users) {
             System.out.println(user);
         }
+    }
+
+    /**
+     * 使用ISelect接口可以参考如下用法(返回值为Page或PageInfo)
+     * desc/aesc
+     */
+    @Test
+    public void testISelect() {
+        //jdk6,7用法，创建接口
+        Page<User> page = PageHelper.startPage(1, 10).setOrderBy("id desc").doSelectPage(new ISelect() {
+            @Override
+            public void doSelect() {
+                userService.getAllUser();
+            }
+        });
+        //jdk8 lambda用法
+        page = PageHelper.startPage(1, 10).setOrderBy("id desc").doSelectPage(() -> userService.getAllUser());
+        //为了说明可以链式使用，上面是单独setOrderBy("id desc")，也可以直接如下
+        page = PageHelper.startPage(1, 10, "id desc").doSelectPage(() -> userService.getAllUser());
+
+        //也可以直接返回PageInfo，注意doSelectPageInfo方法和doSelectPage
+        PageInfo pageInfo = PageHelper.startPage(1, 10).setOrderBy("id desc").doSelectPageInfo(new ISelect() {
+            @Override
+            public void doSelect() {
+                userService.getAllUser();
+            }
+        });
+        //对应的lambda用法
+        pageInfo = PageHelper.startPage(1, 10).setOrderBy("id desc").doSelectPageInfo(() -> userService.getAllUser());
+
+        //count查询，返回一个查询语句的count数
+//        long total = PageHelper.count(new ISelect() {
+//            @Override
+//            public void doSelect() {
+//                userService.selectLike(country);
+//            }
+//        });
+        //lambda
+//        total = PageHelper.count(() -> userService.selectLike(country));
     }
 }
